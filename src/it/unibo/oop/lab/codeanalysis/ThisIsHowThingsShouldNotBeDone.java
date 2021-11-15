80352991ed13bf31b7ba92e01679d68ea5c03430
Carafassi Samuele <samuele.carafassi@studio.unibo.it>
2021-11-15 11:12:39 +0100

    warnings removed

--- src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
+++ src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
@@ -10,17 +10,14 @@ import java.util.Collection;
  * This code triggers static code analyzers. You should use it to see how things
  * SHOULD NOT be done.
  *
- * @author Danilo Pianini
- * @version 1.2
- *
  */
-public class ThisIsHowThingsShouldNotBeDone {
+public final class ThisIsHowThingsShouldNotBeDone {
 
     /*
      * PMD complains:
      * 
-     * This class should be final, because it is a utility class (all methods
-     * are static)
+     * This class should be final, because it is a utility class (all methods are
+     * static)
      * 
      * This class should have a private constructor, because is not meant to be
      * extended (it is an utility class)
@@ -30,6 +27,8 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * This class should not have a public or default constructor
      */
+    private ThisIsHowThingsShouldNotBeDone() {
+    }
 
     /*
      * PMD complains:
@@ -43,13 +42,14 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * Missing Javadoc
      */
-    public static void main(String[] a) {
+    public static void main(final String[] args) {
+        final int numbers = 43;
         /*
          * PMD Complains:
          * 
          * this variable should be final!
          */
-        Collection<Object> c = new ArrayList<>();
+        final Collection<Object> c = new ArrayList<>();
         /*
          * PMD complains:
          * 
@@ -61,14 +61,15 @@ public class ThisIsHowThingsShouldNotBeDone {
          * 
          * What does it mean "43"? It's a magic number!
          */
-        for (int i = 0; i < 43; i++)
+        for (int i = 0; i < numbers; i++) {
             c.add(new Object());
+        }
         /*
          * FindBugs complains
          * 
          * Slow: use clear() instead!
          */
-        c.removeAll(c);
+        c.clear();
     }
 
 }
